8f49d45fe36603cf0a8c5079fde9313e4302c7a3
Nikita Rydanov <wrongwayboyy@gmail.com>
2023-04-17 12:31:43 +0400

    Добавил аннотации типов и улучшил читаемость кода

--- dataset.py
+++ dataset.py
@@ -1,8 +1,143 @@
 import keras
 import librosa
 import numpy as np
+
+from typing import Tuple
+
 from dataclasses import dataclass
 from utils import get_required_for
+
+    
+@dataclass
+class STFTLoader:
+  n_fft: int = 2048
+  hop_length: int = 512
+  freq_bins = 1 + n_fft // 2
+  
+  def transform(self, data: np.ndarray) -> np.ndarray[np.float32]:
+    spec = librosa.stft(y=data, n_fft=self.n_fft,
+                        hop_length=self.hop_length)
+    return spec
+  
+  def process(self, data: np.ndarray)-> np.ndarray[np.float32]:
+    return self.transform(data).swapaxes(0, 1)
+
+@dataclass
+class BaseLoader(keras.utils.Sequence):
+  input_path: str = None
+  output_path: str = None
+  batch_size: int = 32
+  input_size: int = 4410
+  duration: int = None
+  offset: int = 0
+  preemphasis: bool = True
+    
+  def load_data(self) -> Tuple[np.ndarray[np.float32], np.ndarray[np.float32]]:
+    in_data, _ = librosa.load(path=self.input_path,
+      sr=44100,
+      mono=True,
+      offset=self.offset,
+      duration=self.duration)
+    out_data, _ = librosa.load(path=self.output_path,
+      sr=44100,
+      mono=True,
+      offset=self.offset,
+      duration=self.duration)
+
+    if self.preemphasis:
+      in_data = librosa.effects.preemphasis(in_data)
+      out_data = librosa.effects.preemphasis(out_data)
+
+    self.len = in_data.shape[0]
+
+    return in_data, out_data
+  
+  def __post_init__(self) -> None:
+    self.x, self.y = self.load_data()
+
+
+class WindowWaveformLoader(BaseLoader):
+  
+  def load_data(self) -> Tuple[np.ndarray[np.float32], np.ndarray[np.float32]]:
+    
+    in_data, out_data = super().load_data()
+    out_data = out_data[self.input_size - 1:]
+    
+    return in_data, out_data
+    
+  def __getitem__(self, index: int) -> Tuple[np.ndarray[np.float32], 
+                                             np.ndarray[np.float32]]:
+    
+    start = index * self.batch_size
+    end = (index + 1) * self.batch_size
+    
+    x_cur = np.stack([self.x[i:i + self.input_size] for i in range(start, end)])
+    y_cur = self.y[start:end]
+    
+    x_cur = x_cur.reshape(self.batch_size, self.input_size, 1)
+    y_cur = y_cur.reshape(self.batch_size, 1)
+    
+    return x_cur, y_cur 
+  
+  def __len__(self) -> int:
+    return (self.len - self.input_size + 1) // self.batch_size
+
+
+class DirectWaveformLoader(BaseLoader):
+    
+  def __getitem__(self, index: int) -> Tuple[np.ndarray[np.float32], 
+                                             np.ndarray[np.float32]]:
+    
+    start = index * self.batch_size * self.input_size
+    end = (index + 1) * self.batch_size * self.input_size
+    
+    x_cur = np.stack([self.x[i:i + self.input_size] 
+                      for i in range(start, end, self.input_size)])
+    y_cur = np.stack([self.y[i:i + self.input_size] 
+                      for i in range(start, end, self.input_size)])
+    
+    x_cur = x_cur.reshape(self.batch_size, self.input_size, 1)
+    y_cur = y_cur.reshape(self.batch_size, self.input_size, 1)
+    
+    return x_cur, y_cur 
+  
+  def __len__(self) -> int:
+    return self.len // (self.batch_size * self.input_size)
+
+
+class DirectSTFTLoader(DirectWaveformLoader, STFTLoader):
+  
+  def __init__(self, **kwargs) -> None:
+    DirectWaveformLoader.__init__(self, **get_required_for(DirectWaveformLoader,
+                                                           kwargs))
+    STFTLoader.__init__(self, **get_required_for(STFTLoader, kwargs))
+  
+  def __getitem__(self, index: int) -> Tuple[np.ndarray[np.float32], 
+                                             np.ndarray[np.float32]]:
+    x_cur, y_cur = super().__getitem__(index)
+    x_cur = np.apply_along_axis(self.process, 1, x_cur) \
+      .reshape(self.batch_size, -1, self.freq_bins)
+    y_cur = np.apply_along_axis(self.process, 1, y_cur) \
+      .reshape(self.batch_size, -1, self.freq_bins)
+    
+    return np.abs(x_cur), np.abs(y_cur)
+
+
+class WindowSTFTLoader(WindowWaveformLoader, STFTLoader):
+  
+  def __init__(self, **kwargs) -> None:
+    WindowWaveformLoader.__init__(self, **get_required_for(WindowWaveformLoader, 
+                                                           kwargs))
+    STFTLoader.__init__(self, **get_required_for(STFTLoader, kwargs))
+  
+  def __getitem__(self, index: int) -> Tuple[np.ndarray[np.float32],
+                                             np.ndarray[np.float32]]:
+    x_cur, y_cur = super().__getitem__(index)
+
+    x_cur = np.apply_along_axis(self.process, 1, x_cur) \
+      .reshape(self.batch_size, -1, self.freq_bins)
+    
+    return np.abs(x_cur), y_cur
 
 
 @dataclass
@@ -18,7 +153,7 @@ class DataSplitter:
   preemphasis: bool = True
   valid_only: bool = True
 
-  def split(self, cls, **kwargs) -> tuple:
+  def split(self, cls, **kwargs) -> Tuple[BaseLoader]:
     if self.duration is None:
       length = librosa.get_duration(path=self.input_path)
     else:
@@ -49,124 +184,3 @@ class DataSplitter:
       return train, valid, test
     else:
       return train, valid
-    
-@dataclass
-class MelLoader:
-  n_fft: int = 2048
-  sr: int = 44100
-  hop_length: int = 512
-  n_mels: int = 128
-  fmax: int = 8000
-  
-  def convert_to_mel(self, data: np.ndarray):
-    spec = librosa.feature.melspectrogram(y=data, sr=self.sr, n_fft=self.n_fft,
-                                          hop_length=self.hop_length, 
-                                          n_mels=self.n_mels, fmax=self.fmax)
-
-    spec_db = librosa.power_to_db(spec, ref=np.max)
-    return spec_db
-  
-  def process(self, data: np.ndarray):
-    return self.convert_to_mel(data).transpose()
-
-@dataclass
-class BaseLoader(keras.utils.Sequence):
-  input_path: str = None
-  output_path: str = None
-  batch_size: int = 32
-  input_size: int = 4410
-  duration: int = None
-  offset: int = 0
-  preemphasis: bool = True
-    
-  def load_data(self):
-
-    in_data, _ = librosa.load(path=self.input_path,
-      sr=44100,
-      mono=True,
-      offset=self.offset,
-      duration=self.duration)
-
-    out_data, _ = librosa.load(path=self.output_path,
-      sr=44100,
-      mono=True,
-      offset=self.offset,
-      duration=self.duration)
-
-    if self.preemphasis:
-      in_data = librosa.effects.preemphasis(in_data)
-      out_data = librosa.effects.preemphasis(out_data)
-
-    self.len = in_data.shape[0]
-
-    return in_data, out_data
-  
-  def __post_init__(self) -> None:
-        
-    self.x, self.y = self.load_data()
-    
-class WindowWaveformLoader(BaseLoader):
-  
-  def load_data(self):
-    
-    in_data, out_data = super().load_data()
-    
-    out_data = out_data[self.input_size - 1:]
-    
-    return in_data, out_data
-    
-  def __getitem__(self, index: int) -> tuple:
-    
-    start = index * self.batch_size
-    end = (index + 1) * self.batch_size
-    
-    x_cur = np.stack([self.x[i:i + self.input_size] for i in range(start, end)])
-    y_cur = self.y[start:end]
-    
-    x_cur = x_cur.reshape(self.batch_size, self.input_size, 1)
-    y_cur = y_cur.reshape(self.batch_size, 1)
-    
-    return x_cur, y_cur 
-  
-  def __len__(self) -> int:
-    return (self.len - self.input_size + 1) // self.batch_size
-
-class DirectWaveformLoader(BaseLoader):
-    
-  def __getitem__(self, index: int) -> tuple:
-    
-    start = index * self.batch_size * self.input_size
-    end = (index + 1) * self.batch_size * self.input_size
-    
-    x_cur = np.stack([self.x[i:i + self.input_size] for i in range(start, end, self.input_size)])
-    y_cur = np.stack([self.y[i:i + self.input_size] for i in range(start, end, self.input_size)])
-    
-    x_cur = x_cur.reshape(self.batch_size, self.input_size, 1)
-    y_cur = y_cur.reshape(self.batch_size, self.input_size, 1)
-    
-    return x_cur, y_cur 
-  
-  def __len__(self) -> int:
-    return self.len // (self.batch_size * self.input_size)
-  
-class DirectMelLoader(DirectWaveformLoader, MelLoader):
-  def __init__(self, **kwargs):
-
-    DirectWaveformLoader.__init__(self, **get_required_for(DirectWaveformLoader, kwargs))
-    MelLoader.__init__(self, **get_required_for(MelLoader, kwargs))
-  
-  def __getitem__(self, index: int) -> tuple:
-    
-    start = index * self.batch_size * self.input_size
-    end = (index + 1) * self.batch_size * self.input_size
-    
-    x_cur = np.stack([self.x[i:i + self.input_size] for i in range(start, end, self.input_size)])
-    y_cur = np.stack([self.y[i:i + self.input_size] for i in range(start, end, self.input_size)])
-    
-    x_cur = x_cur.reshape(self.batch_size, self.input_size, 1)
-    y_cur = y_cur.reshape(self.batch_size, self.input_size, 1)
-    
-    x_cur = np.apply_along_axis(self.process, 1, x_cur).reshape(self.batch_size, self.n_mels, -1)
-    y_cur = np.apply_along_axis(self.process, 1, y_cur).reshape(self.batch_size, self.n_mels, -1)
-    
-    return x_cur, y_cur
--- utils.py
+++ utils.py
@@ -3,19 +3,25 @@ import git
 import os
 from keras import Model
 from dataclasses import fields
+import numpy as np
+from librosa import istft
 
-def save_attempt_data(path: str, model: Model):
+from typing import Dict
+
+def save_attempt_data(path: str, model: Model) -> None:
   if not os.path.exists(path):
       os.makedirs(path)
 
   repo = git.Repo(search_parent_directories=True)
-
   data = {'commit': repo.head.object.hexsha}
-
-  f = open(f"{path}/history.json", 'w')
-
-  json.dump(data, f)
   
-  f.close()  
-def get_required_for(cls, kwargs):
-  return {k : v for k, v in kwargs.items() if k in list(map(lambda x: x.name, fields(cls)))}
+  with open(f"{path}/history.json", 'w') as f:
+    json.dump(data, f)
+
+def get_required_for(cls, kwargs) -> Dict[str, object]:
+  return {k : v for k, v in kwargs.items() 
+          if k in list(map(lambda x: x.name, fields(cls)))}
+
+def output_to_audio(data: np.ndarray, **kwargs) -> np.ndarray[np.float32]:
+  data = data.swapaxes(1, 2)
+  return np.stack([istft(x, **kwargs)] for x in data)
